c142278d25148dc77b5c94deb7fc82b86d009332
PabloMayochi <pablomayochi@gmail.com>
2021-08-07 16:10:06 -0300

    Commit Scrapeo Intento 2

--- TP 2 - Scrapping.R
+++ TP 2 - Scrapping.R
@@ -1,3 +1,14 @@
 library(rvest)
+library(tidyverse)
 
-read_html(coinbase.com/price)
+url <- "https://www.coinbase.com/es-LA/price"
+
+coinbase <- read_html(url)
+
+coinbase
+
+coinbase_table <- coinbase %>% 
+  html_nodes(xpath = '//*[@id="main"]/div[1]/table') %>% 
+  html_table(fill = T)
+
+coinbase_table[[1]]
