d8c5ff1012bede4fd3455973911d63a8cfb8d481
PabloMayochi <pablomayochi@gmail.com>
2021-08-13 20:20:47 -0300

    13/08

--- TP 2 - Scrapping.R
+++ TP 2 - Scrapping.R
@@ -1,14 +1,53 @@
 library(rvest)
 library(tidyverse)
 
-url <- "https://www.coinbase.com/es-LA/price"
+crpyto_url <- read_html("https://www.tradingview.com/markets/cryptocurrencies/prices-all/")
 
-coinbase <- read_html(url)
+crypt_table <- html_nodes(crpyto_url, css = "table")
 
-coinbase
+crypt_table <- html_table(crypt_table, fill = T) %>% 
+  as.data.frame()
 
-coinbase_table <- coinbase %>% 
-  html_nodes(xpath = '//*[@id="main"]/div[1]/table') %>% 
-  html_table(fill = T)
+crypt_table$Var.2 <- gsub("B","", crypt_table$Var.2)
+crypt_table$Var.2 <- gsub("M","", crypt_table$Var.2)
+crypt_table$Var.2 <- gsub("K","", crypt_table$Var.2)
+crypt_table$Var.3 <- gsub("B","", crypt_table$Var.3)
+crypt_table$Var.3 <- gsub("M","", crypt_table$Var.3)
+crypt_table$Var.3 <- gsub("K","", crypt_table$Var.3)
+crypt_table$Var.4 <- gsub("B","", crypt_table$Var.2)
+crypt_table$Var.4 <- gsub("M","", crypt_table$Var.4)
+crypt_table$Var.4 <- gsub("K","", crypt_table$Var.4)
+crypt_table$Var.5 <- gsub("B","", crypt_table$Var.5)
+crypt_table$Var.5 <- gsub("M","", crypt_table$Var.5)
+crypt_table$Var.5 <- gsub("K","", crypt_table$Var.5)
+crypt_table$Var.6 <- gsub("B","", crypt_table$Var.6)
+crypt_table$Var.6 <- gsub("M","", crypt_table$Var.6)
+crypt_table$Var.6 <- gsub("K","", crypt_table$Var.6)
+crypt_table$Var.7 <- gsub("B","", crypt_table$Var.7)
+crypt_table$Var.7 <- gsub("M","", crypt_table$Var.7)
+crypt_table$Var.7 <- gsub("K","", crypt_table$Var.7)
+crypt_table$Var.8 <- gsub("B","", crypt_table$Var.8)
+crypt_table$Var.8 <- gsub("M","", crypt_table$Var.8)
+crypt_table$Var.8 <- gsub("K","", crypt_table$Var.8)
+crypt_table$Var.8 <- gsub("%","", crypt_table$Var.8)
 
-coinbase_table[[1]]
+x <- c("Currency","Mkt Cap", "Fd Mkt Cap", "Last", "Avail coins", 
+       "Total coins", "Traded Vol", "Chng%")
+
+names(crypt_table) <- x
+
+crypt_table <- 
+  data.frame(apply(crypt_table[2:8],2, as.numeric)) %>% 
+  cbind(currency = crypt_table$Currency) %>% 
+  select(8,1:7)
+
+crypt_table %>% 
+  select(currency, Traded.Vol) %>% 
+  filter(Traded.Vol > 600) %>% 
+  ggplot(., aes(reorder(currency, Traded.Vol), Traded.Vol )) +
+  geom_bar(stat = "identity", aes(fill = Traded.Vol)) + coord_flip() +
+  ggtitle("Top 10 Traded Volume") +
+  xlab("Currency") + ylab("Traded Volume") + 
+  theme(plot.title = element_text(hjust = 0.5, face = "bold", size = 15),
+        axis.title.x = element_text(face = "bold", size =  13),
+        axis.title.y = element_text(face = "bold", size = 13))
